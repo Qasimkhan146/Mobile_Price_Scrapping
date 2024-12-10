e922274c0dba135f10a224e8dc736e63a764c11f
MAGMA3C <magma3c.com@gmail.com>
2024-12-10 15:34:02 +0500

    qasim khan changes

--- backend/config/mongoDBconfig.js
+++ backend/config/mongoDBconfig.js
@@ -5,7 +5,9 @@ dotenv.config();
 
 const connectDB = async () =>{
     try{
-        await mongoose.connect("mongodb://uzair:Uzair1234@192.168.10.86:7842/mobilePrice")
+        // await mongoose.connect("mongodb://uzair:Uzair1234@192.168.10.86:7842/mobilePrice")
+        await mongoose.connect('mongodb://uzair:Uzair1234@116.202.124.91:7842/mobilePrice',);
+
         console.log("MongoDB connected")
     }catch(error){
         console.log(error)
