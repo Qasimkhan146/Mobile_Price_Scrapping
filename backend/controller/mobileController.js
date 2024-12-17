11c7d686bb7ebc59c4cdac17cf5bbbffc0e9a819
QasimKhan146 <qasimkhan1243@gmail.com>
2024-12-17 16:25:23 +0500

    qasimkhan changes

--- backend/controller/mobileController.js
+++ backend/controller/mobileController.js
@@ -1,23 +1,26 @@
 import Mobile from "../model/mobileModel.js";
 import Price from "../model/priceModel.js";
-export const fetchSingleMobilePrice =async (req, res) => {
+
+export const fetchSingleMobilePrice = async (req, res) => {
   try {
     const { model } = req.params;
-    const mobile =await Mobile.findOne({ model: new RegExp(model, "i") });
-    if(!mobile){
-        res.status(404).send("mobile not found")
+    const mobile = await Mobile.findOne({ model: new RegExp(model, "i") });
+    const prices = await Price.find({ model: new RegExp(model, "i") });
+    if (!mobile) {
+      return res.status(404).send("Mobile not found");
     }
+    if (!prices.length) {
+      return res.status(404).send("Price not found");
+    }
+    const priceDetails = prices.map((price) => ({
+      source: price.source,
+      price: price.price,
+      href: price.href,
+    }));
 
-    const price  = await Price.findOne({model: new RegExp(model, "i")});
-    if(!price){
-        res.status(404).send("price not found")
-    }
-    res.status(200).send(mobile && price.map((price) => ({
-        source: price.source,
-        price: price.price,
-        href: price.href,
-      })),);
+    res.status(200).send({ mobile, prices: priceDetails });
   } catch (error) {
     res.status(500).send(error.message);
   }
 };
+
