ec34c2080ac830b1bf4f39c097e67ae2fe990109
MAGMA3C <magma3c.com@gmail.com>
2024-12-31 12:04:52 +0500

    changes by qasim

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ connectDB();
 
 const app = express();
 app.use(cors({
-    origin: "http://localhost:3000", // Frontend URL
+    origin: "*", // Frontend URL
     credentials: true,              // Allow cookies
     methods: ["GET", "POST"],       // Allowed methods
     allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers
