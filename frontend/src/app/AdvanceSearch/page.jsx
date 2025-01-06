c05089584ed627b5a893d730258393fb08be4ea9
MAGMA3C <magma3c.com@gmail.com>
2025-01-06 20:12:11 +0500

    qasim khan changes

--- frontend/src/app/AdvanceSearch/page.jsx
+++ frontend/src/app/AdvanceSearch/page.jsx
@@ -3,42 +3,42 @@ import React, { Suspense } from "react";
 
 import AdvanceSearchComponent from "../component/AdvanceSearchComponent/AdvanceSearchComponent";
 
-export async function generateMetaData({params}) {
-  // const {slug} = params;
-  let pageTitle = "Advance Search for Mobile | Compare, ExPLORE & Buy Smartphones";
+// export async function generateMetaData({params}) {
+//   // const {slug} = params;
+//   let pageTitle = "Advance Search for Mobile | Compare, ExPLORE & Buy Smartphones";
 
-  return {
-    title: `${pageTitle}`,
-    description: "Find the perfect mobile for your needs with our Advance Search feature. Compare mobile rates, explore mobile specs, and get the Latest prices on All Vendors.",
-    // Open Graph (OG) tags for Facebook and LinkedIn
-    openGraph: {
-      title: `${pageTitle}`,
-      description: "Find the perfect mobile for your needs with our Advance Search feature. Compare mobile rates, explore mobile specs, and get the Latest prices on All Vendors.",
-      url: "https://mobileprice.biz.pk/AdvanceSearch",
-      type: "website",
-      images: [
-        {
-          url: "https://mobileprice.biz.pk/images/MOBILE PRI.png",
-          width: 800,
-          height: 600,
-          alt: `${pageTitle} image`,
-        },
-      ],
-    },
+//   return {
+//     title: `${pageTitle}`,
+//     description: "Find the perfect mobile for your needs with our Advance Search feature. Compare mobile rates, explore mobile specs, and get the Latest prices on All Vendors.",
+//     // Open Graph (OG) tags for Facebook and LinkedIn
+//     openGraph: {
+//       title: `${pageTitle}`,
+//       description: "Find the perfect mobile for your needs with our Advance Search feature. Compare mobile rates, explore mobile specs, and get the Latest prices on All Vendors.",
+//       url: "https://mobileprice.biz.pk/AdvanceSearch",
+//       type: "website",
+//       images: [
+//         {
+//           url: "https://mobileprice.biz.pk/images/MOBILE PRI.png",
+//           width: 800,
+//           height: 600,
+//           alt: `${pageTitle} image`,
+//         },
+//       ],
+//     },
     
-    // Twitter card tags
-    twitter: {
-      card: "summary_large_image",
-      title: `${pageTitle} price in Pakistan`,
-      description: "Find the perfect mobile for your needs with our Advance Search feature. Compare mobile rates, explore mobile specs, and get the Latest prices on All Vendors.",
-      image:"https://mobileprice.biz.pk/images/MOBILE PRI.png",
-    },
-    alternates: {
-      canonical: "https://mobileprice.biz.pk/images/MOBILE PRI.png",
-   },
- };
+//     // Twitter card tags
+//     twitter: {
+//       card: "summary_large_image",
+//       title: `${pageTitle} price in Pakistan`,
+//       description: "Find the perfect mobile for your needs with our Advance Search feature. Compare mobile rates, explore mobile specs, and get the Latest prices on All Vendors.",
+//       image:"https://mobileprice.biz.pk/images/MOBILE PRI.png",
+//     },
+//     alternates: {
+//       canonical: "https://mobileprice.biz.pk/images/MOBILE PRI.png",
+//    },
+//  };
 
-}
+// }
 const AdvanceSearch = () => {
   return (
     <Suspense fallback={<div>Loading...</div>}>
--- frontend/src/app/Mobile/[slug]/page.jsx
+++ frontend/src/app/Mobile/[slug]/page.jsx
@@ -2,52 +2,52 @@
 import React from 'react'
 import PhoneDetail from '../../component/PhoneDetail/PhoneDetail';
 
-export async function generateMetaData({params}) {
-  const {slug} = params;
-  let pageTitle = "Detail";
-  let pageDetail = {};
-  try{
-   const slugFormated = slug ? slug.split("-").join(" ") : "";
-   const res = await fetch(`https://7842.mobileprice.biz.pk/mobile/fetchSingleMobile/${slugFormated}}`);
-   if(!res.ok){
-     return {
-      title: pageTitle,
-      description: "",
-      openGraph:{},
-      twitter:{}
-     }
-     const brandData = await res.json();
-     pageTitle = brandData.model;
-     pageDetail = brandData;
-   }
-  }catch(err){
-     console.error(err);
-  }
-  return{
-    title: `${pageTitle} price in Pakistan`,
-    description: `${pageTitle} mobile specification RAM ${pageDetail.Ram}, ROM ${pageDetail.Rom}, Front Camera ${pageDetail.front_Cam} price in Pakistan, Back Camera ${pageDetail.Back_Cam} price in Pakistan, Battery: ${pageDetail.Capacity} MAH, Screen ${pageDetail.Size} IN, OS ${pageDetail.OS} ....`,
-    openGraph: {
-      title: `${pageTitle} price in Pakistan`,
-      description: `${pageTitle} mobile specification RAM ${pageDetail.Ram}, ROM ${pageDetail.Rom}, Front Camera ${pageDetail.front_Cam} price in Pakistan, Back Camera ${pageDetail.Back_Cam} price in Pakistan, Battery: ${pageDetail.Capacity} MAH, Screen ${pageDetail.Size} IN, OS ${pageDetail.OS} ....`,
-      url: `https://mobileprice.biz.pk/Mobile/${slug}`,
-      type: "website",
-      images: [
-        {
-          url: pageDetail.img_url_mobilemate || "https://mobileprice.biz.pk/images/MOBILE PRI.png",
-          width: 800,
-          height: 600,
-          alt: `${pageTitle} image`,
-        },
-      ] 
-    },
-    twitter:{
-      card: "summary_large_image",
-      title: `${pageTitle} price in Pakistan`,
-      description: `${pageTitle} mobile specification RAM ${pageDetail.Ram}, ROM ${pageDetail.Rom}, Front Camera ${pageDetail.front_Cam} price in Pakistan, Back Camera ${pageDetail.Back_Cam} price in Pakistan, Battery: ${pageDetail.Capacity} MAH, Screen ${pageDetail.Size} IN, OS ${pageDetail.OS} ....`,
-      image: pageDetail.img_url_mobilemate || "https://mobileprice.biz.pk/images/MOBILE PRI.png"
-    }
-  }
-}
+// export async function generateMetaData({params}) {
+//   const {slug} = params;
+//   let pageTitle = "Detail";
+//   let pageDetail = {};
+//   try{
+//    const slugFormated = slug ? slug.split("-").join(" ") : "";
+//    const res = await fetch(`https://7842.mobileprice.biz.pk/mobile/fetchSingleMobile/${slugFormated}}`);
+//    if(!res.ok){
+//      return {
+//       title: pageTitle,
+//       description: "",
+//       openGraph:{},
+//       twitter:{}
+//      }
+//      const brandData = await res.json();
+//      pageTitle = brandData.model;
+//      pageDetail = brandData;
+//    }
+//   }catch(err){
+//      console.error(err);
+//   }
+//   return{
+//     title: `${pageTitle} price in Pakistan`,
+//     description: `${pageTitle} mobile specification RAM ${pageDetail.Ram}, ROM ${pageDetail.Rom}, Front Camera ${pageDetail.front_Cam} price in Pakistan, Back Camera ${pageDetail.Back_Cam} price in Pakistan, Battery: ${pageDetail.Capacity} MAH, Screen ${pageDetail.Size} IN, OS ${pageDetail.OS} ....`,
+//     openGraph: {
+//       title: `${pageTitle} price in Pakistan`,
+//       description: `${pageTitle} mobile specification RAM ${pageDetail.Ram}, ROM ${pageDetail.Rom}, Front Camera ${pageDetail.front_Cam} price in Pakistan, Back Camera ${pageDetail.Back_Cam} price in Pakistan, Battery: ${pageDetail.Capacity} MAH, Screen ${pageDetail.Size} IN, OS ${pageDetail.OS} ....`,
+//       url: `https://mobileprice.biz.pk/Mobile/${slug}`,
+//       type: "website",
+//       images: [
+//         {
+//           url: pageDetail.img_url_mobilemate || "https://mobileprice.biz.pk/images/MOBILE PRI.png",
+//           width: 800,
+//           height: 600,
+//           alt: `${pageTitle} image`,
+//         },
+//       ] 
+//     },
+//     twitter:{
+//       card: "summary_large_image",
+//       title: `${pageTitle} price in Pakistan`,
+//       description: `${pageTitle} mobile specification RAM ${pageDetail.Ram}, ROM ${pageDetail.Rom}, Front Camera ${pageDetail.front_Cam} price in Pakistan, Back Camera ${pageDetail.Back_Cam} price in Pakistan, Battery: ${pageDetail.Capacity} MAH, Screen ${pageDetail.Size} IN, OS ${pageDetail.OS} ....`,
+//       image: pageDetail.img_url_mobilemate || "https://mobileprice.biz.pk/images/MOBILE PRI.png"
+//     }
+//   }
+// }
 const page = () => {
   return (
     <div>
