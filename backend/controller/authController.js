db4944f7bfd610ba48be9db620e0e8e4b3904f90
QasimKhan146 <qasimkhan1243@gmail.com>
2025-01-13 12:13:14 +0500

    qasim khan changes

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -16,34 +16,30 @@ export const registerUser = async (req, res) => {
 export const loginUser = async (req, res) => {
     try {
         const { email, password } = req.body;
-        console.log("Request Body:", req.body);
+
 
         const user = await User.findOne({ email });
         if (!user) {
-            console.log("User not found");
             return res.status(404).json({ message: "No user found" });
         }
 
         const isMatch = await bcrypt.compare(password, user.password);
         if (!isMatch) {
-            console.log("Password mismatch");
             return res.status(401).json({ message: "Authentication failed: Incorrect credentials" });
         }
 
         const token = jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET, {
-            expiresIn: "1d", // Token expires in 1 day
+            expiresIn: "1h", // Token expires in 1 hour
         });
-        console.log("Generated Token:", token);
 
         // Set cookie with correct options
         res.cookie("token", token, {
             httpOnly: true, // Secure cookie
             secure: process.env.NODE_ENV === "production", // Use secure flag in production
             sameSite: process.env.NODE_ENV === "production" ? "none" : "lax", // Adjust for cross-origin requests
-            maxAge: 1000 * 60 * 60 * 24, // 1 day
+            maxAge: 3600 * 1000, // 1 hour in milliseconds
         });
-
-        console.log("Token set in cookie");
+        
         return res.status(200).json({ user, token });
     } catch (error) {
         console.error("Error in loginUser:", error);
