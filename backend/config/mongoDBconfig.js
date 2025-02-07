89c27cd78eda6d28420f2dc9a6f2ae153d2df91b
QasimKhan146 <qasimkhan1243@gmail.com>
2025-02-07 16:01:31 +0500

    qasim

--- backend/config/mongoDBconfig.js
+++ backend/config/mongoDBconfig.js
@@ -6,7 +6,8 @@ dotenv.config();
 const connectDB = async () =>{
     try{
         // await mongoose.connect('mongodb://localhost:27017/mobilePrice',);
-        await mongoose.connect('mongodb://192.168.10.86:7842/mobileprice',);
+        // await mongoose.connect('mongodb://192.168.10.86:7842/mobileprice',);
+        await mongoose.connect('mongodb://mp.magma3c.com:7842/mobileprice',);
 
         console.log("MongoDB connected")
     }catch(error){
