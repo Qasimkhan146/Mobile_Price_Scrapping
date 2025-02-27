5b4530eac6825798271a8c4dc3a08d4eff3aaaba
QasimKhan146 <qasimkhan1243@gmail.com>
2025-02-27 13:10:41 +0500

    qasim khan changes

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -17,22 +17,6 @@ export default function RootLayout({ children }) {
         />
       </head>
       <body className="container">
-        {/* Add Google Analytics Scripts */}
-        {/* <!-- Google tag (gtag.js) --> */}
-        <Script
-          async
-          src={`https://www.googletagmanager.com/gtag/js?id=G-1111111`}
-        />
-        <Script>
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-
-            gtag('config', 'G-2NFWB379HP');
-          `}
-        </Script>
-
         <ClientLayout>
           <Header />
           {children}
